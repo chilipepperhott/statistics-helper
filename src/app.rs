8f3a75068afe261273e3dfc1cc7550de2eb38606
Elijah Potter <elijah.sirius@protonmail.com>
2021-03-03 19:18:50 -0700

    Started adding more graphing features

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,18 @@
+use std::collections::HashMap;
 use eframe::egui::plot::*;
 use eframe::{egui::*, epi};
+
+#[derive(PartialEq, Copy, Clone)]
+enum XAxis{
+    Index,
+    ZScore
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum YAxis{
+    Value,
+    Frequency
+}
 
 pub struct App {
     data_string: String,
@@ -9,6 +22,8 @@ pub struct App {
     variance: f64,
     standard_deviation: f64,
     is_sample: bool,
+    x: XAxis,
+    y: YAxis,
     plot: Plot,
 }
 
@@ -22,6 +37,8 @@ impl App {
             variance: 0.0,
             standard_deviation: 0.0,
             is_sample: false,
+            x: XAxis::Index,
+            y: YAxis::Value,
             plot: Plot::default(),
         }
     }
@@ -38,6 +55,20 @@ impl epi::App for App {
             });
             ui.checkbox(&mut self.is_sample, "Is sample data");
 
+            ui.horizontal(|ui|{
+            ui.group(|ui|{
+                ui.label("X Axis:");
+                ui.radio_value(&mut self.x, XAxis::Index, "Index");
+                ui.radio_value(&mut self.x, XAxis::ZScore, "ZScore");
+            });
+
+            ui.group(|ui|{
+                ui.label("Y Axis:");
+                ui.radio_value(&mut self.y, YAxis::Frequency, "Frequency");
+                ui.radio_value(&mut self.y, YAxis::Value, "Value");
+            });
+        });
+
             if self.data_enter_err {
                 ui.add(Label::new("Could not parse data").text_color(Color32::RED));
             }
@@ -48,7 +79,7 @@ impl epi::App for App {
                         self.data_enter_err = false;
 
                         let (data, mean, median, variation, standard_deviation) =
-                            plot_data(&d, self.is_sample);
+                            plot_data(&d, self.is_sample, self.x, self.y);
 
                         self.data_enter_err = false;
                         self.plot = Plot::default().curve(Curve::from_values(data));
@@ -85,8 +116,8 @@ impl epi::App for App {
     }
 }
 
-/// Plots the data, returning the points, mean, median, variance and standard deviation
-fn plot_data(data: &Vec<f64>, is_sample: bool) -> (Vec<Value>, f64, f64, f64, f64) {
+/// Plots the data, returning the points, mean, median, variance and standard deviation. If provided y axis is frequency, x is alwaus z-score
+fn plot_data(data: &Vec<f64>, is_sample: bool, x: XAxis, y: YAxis) -> (Vec<Value>, f64, f64, f64, f64) {
     let mut owned = data.to_owned();
     owned.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
@@ -117,8 +148,18 @@ fn plot_data(data: &Vec<f64>, is_sample: bool) -> (Vec<Value>, f64, f64, f64, f6
     // Construct the final output points
     let mut output = Vec::with_capacity(data.len());
 
-    for (p, v) in data.iter().enumerate() {
-        output.push(Value::new(p as f64, *v))
+    if y == YAxis::Frequency{
+        let frequencies = get_frequencies(owned.as_slice());
+
+    }else{
+        for (p, v) in data.iter().enumerate(){
+            if x == XAxis::Index{
+                output.push(Value::new(p as f64, *v));
+            }
+            else{
+                output.push(Value::new((*v as f64 - mean) / standard_deviation, *v));
+            }
+        }
     }
 
     (output, mean, median, variation, standard_deviation)
@@ -152,6 +193,22 @@ fn filter(s: &String) -> String {
         let last = output.pop().unwrap();
         if last != ',' {
             output.push(last)
+        }
+    }
+
+    output
+}
+
+/// Counts how many times a value exists in a vec, removing them as it goes
+fn get_frequencies(d: &[f64]) -> HashMap<u64, usize>{
+    let mut output: HashMap<u64, usize> = HashMap::new();
+    
+    for i in d{
+        let bits = (*i).to_bits();
+        if output.contains_key(&bits){
+            output[&bits] += 1;
+        }else{
+            output.insert(bits, 1);
         }
     }
 
